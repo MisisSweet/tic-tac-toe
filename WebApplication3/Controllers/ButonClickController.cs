0ee933905842a749e1c528b38a1160a786bd2895
MisisSweet <48993011+MisisSweet@users.noreply.github.com>
2021-02-18 22:43:01 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/ButonClickController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class ButonClickController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult onTapFindGame()
+        {
+            return Redirect("~/FindGame");
+        }
+        public IActionResult onTapCreateGame()
+        {
+            return Redirect("~/CreateGame");
+        }
+    }
+}
---
+++ WebApplication3/Controllers/CreateGameController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CreateGameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult onTapCanlce()
+        {
+            return Redirect("~/Home");
+        }
+        public IActionResult onTapCreate(string name, Tag[] tags)
+        {
+            return Redirect("~/Game");
+        }
+    }
+}
